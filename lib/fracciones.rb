dbd368aad62512a8f076464a59597524c960ceb4
Alan92 <alan_92_@hotmail.es>
2012-11-06 23:04:42 +0000

    Sexto commit

--- lib/fracciones.rb
+++ lib/fracciones.rb
@@ -2,8 +2,18 @@ class Frac
 attr_accessor :num, :denom
 
 def initialize(x,y)
-@num=x
-@denom=y
+@num=x/mcd(x,y)
+@denom=y/mcd(x,y)
+end
+
+def mcd(x,y)
+d = x.abs, y.abs #[a.abs, b.abs]
+while d.min != 0
+d = d.min, d.max%d.min
+end
+return d.max
 end
 
 end
+
+
--- spec/fracciones_spec.rb
+++ spec/fracciones_spec.rb
@@ -2,6 +2,7 @@ require 'fracciones'
 describe Frac do
 before :each do
 @frac_obj_1=Frac.new(1,0)
+@frac_obj_2=Frac.new(2,4)
 end
 
 it " Debe existir un numerador" do
@@ -12,6 +13,10 @@ it " Debe existir un denominador" do
 @frac_obj_1.denom.should==0
 end
 
+it "Debe de estar en su forma reducida" do
+@frac_obj_2.num.should==1
+@frac_obj_2.denom.should==2
+end
 
 end
 
