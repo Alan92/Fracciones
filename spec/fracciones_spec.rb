c16dfa873cfa7927e2e6a66ce6ff8349a3f00760
Alan92 <alan_92_@hotmail.es>
2012-11-06 23:10:31 +0000

    Octavo commit

--- spec/fracciones_spec.rb
+++ spec/fracciones_spec.rb
@@ -1,16 +1,16 @@
 require 'fracciones'
 describe Frac do
 before :each do
-@frac_obj_1=Frac.new(1,0)
+@frac_obj_1=Frac.new(0,1)
 @frac_obj_2=Frac.new(2,4)
 end
 
 it " Debe existir un numerador" do
-@frac_obj_1.num.should==1
+@frac_obj_1.num.should==0
 end
 
 it " Debe existir un denominador" do
-@frac_obj_1.denom.should==0
+@frac_obj_1.denom.should==1
 end
 
 it "Debe de estar en su forma reducida" do
@@ -19,8 +19,13 @@ it "Debe de estar en su forma reducida" do
 end
 
 it "Se debe invocar al metodo num() para obtener el numerador" do
-@frac_obj_1.num.should==1
+@frac_obj_1.num.should==0
 end
+
+it "Se debe invocar al metodo denom() para obtener el numerador" do
+@frac_obj_1.denom.should==1
+end
+
 end
 
 
